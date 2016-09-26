c1af8ea9bbf21087443b5d2d4c1dd39f49ca6608
Andre Wade <andrewade77@yahoo.com>
2016-09-26 10:32:10 -0400

    Section 2 lab
    AYYYYYYYYYYYYYYYYy

---
+++ ConsoleApplication4/ConsoleApplication4/Person.cs
@@ -0,0 +1,72 @@
+﻿namespace ConsoleApplication4
+{
+    class Person
+    {
+        public string FirstName;
+        public string LastName;
+        public Person Spouse;
+        public int Age;
+        public static int OfPeople;
+        public static int SumOfAllAges;
+
+        public static double AverageAgeOfEveryone()
+        {
+            return Person.SumOfAllAges / Person.OfPeople;
+
+        }
+
+
+
+        public void InformationNeeded()
+        {
+            System.Console.Write("First Name:   ");
+            FirstName = System.Console.ReadLine();
+
+            System.Console.Write("Last Name:    ");
+            LastName = System.Console.ReadLine();
+
+            System.Console.Write("Age:          ");
+            Age = int.Parse(System.Console.ReadLine());
+
+            Person.OfPeople++;
+            Person.SumOfAllAges += Age;
+
+        }
+        public void SpouseInformation()
+        {
+            this.Spouse = new Person();
+
+            System.Console.Write("Spouse Name:  ");
+            Spouse.FirstName = System.Console.ReadLine();
+
+            System.Console.Write("Spouse Age:   ");
+            Spouse.Age = int.Parse(System.Console.ReadLine());
+
+            Person.OfPeople++;
+            Person.SumOfAllAges += Age;
+        }
+        public string GetFullName()
+        {
+            return FirstName + " " + LastName;
+        
+
+        }
+        public string GetSpouseFullName()
+        {
+            return Spouse.FirstName + " " + this.LastName;
+
+        }
+        public void PrintNameAndAge()
+        {
+            System.Console.WriteLine(this.GetFullName());
+            System.Console.WriteLine ("{"this.Age"}");
+            System.Console.WriteLine(this.GetSpouseFullName());
+            System.Console.WriteLine("{"this.Spouse.Age"}");
+
+
+
+        }
+
+        
+    }
+}
---
+++ ConsoleApplication4/ConsoleApplication4/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { Person p1 = new Person();
+            Person p2 = new Person();
+
+            System.Console.WriteLine("(======Information on p1======)");
+            System.Console.WriteLine("                               ");
+
+            p1.InformationNeeded();
+            p1.SpouseInformation();
+
+            System.Console.WriteLine("                               ");
+
+            System.Console.WriteLine("(======Information on p2======)");
+            System.Console.WriteLine("                               ");
+
+            p2.InformationNeeded();
+            p2.SpouseInformation();
+
+            System.Console.WriteLine("                               ");
+
+            System.Console.WriteLine("(======Summary======)");
+            System.Console.WriteLine("                               ");
+
+            p1.PrintNameAndAge();
+            p2.PrintNameAndAge();
+
+            System.Console.WriteLine(Person.AverageAgeOfEveryone());
+
+        }
+    }
+}
