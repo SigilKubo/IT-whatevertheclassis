0f3e47cdda3e45ea9491398bd6273dd8897d4587
Andre Wade <andrewade77@yahoo.com>
2016-11-08 16:50:31 -0500

    ughgigngjogog
    o

---
+++ POOOOOOOOOOOOOOOOPPP COOOOORN/POOOOOOOOOOOOOOOOPPP COOOOORN/FancyStuff.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POOOOOOOOOOOOOOOOPPP_COOOOORN
+{
+    class FancyStuff
+    {
+        public static void Typehere()
+        {
+            System.Console.WriteLine();
+            System.Console.Write("  ===>  ");
+
+        }
+        public static void StoreClerk(string text)
+        {
+            System.Console.WriteLine();
+            System.Console.ForegroundColor = System.ConsoleColor.Yellow;
+            System.Console.WriteLine(" " + text);
+            System.Console.WriteLine();
+            System.Console.Write(" ");
+            System.Console.ResetColor();
+
+        }
+
+
+        
+        public static void Question()
+        {
+            FancyStuff.StoreClerk("Hi Welcome to the movies. Will this be a day or an evening movie?");
+            
+
+        }
+        
+
+        public static void Endencounter()
+        {
+            FancyStuff.StoreClerk(" Thank you for choosing ChiraqBangbang Cinema! ");
+            Console.WriteLine("Press Any Key to continue...");
+            System.Console.ReadKey();
+
+        }
+    }
+
+}
---
+++ POOOOOOOOOOOOOOOOPPP COOOOORN/POOOOOOOOOOOOOOOOPPP COOOOORN/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POOOOOOOOOOOOOOOOPPP_COOOOORN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FancyStuff.Question();
+            FancyStuff.Typehere();
+
+            string Answer = System.Console.ReadLine();
+
+            if (Answer.ToLower().Contains("morning") || Answer.ToLower().Contains("daytime"))
+            {
+
+                FancyStuff.StoreClerk("Okay, A daytime movie, that's great!");
+                FancyStuff.StoreClerk("Will you be bringing any kids?");
+                FancyStuff.Typehere();
+                int NumberofKids = int.Parse(System.Console.ReadLine());
+                FancyStuff.StoreClerk("All right. How Many Adults?");
+                FancyStuff.Typehere();
+                int NumberofAdults = int.Parse(System.Console.ReadLine());
+                FancyStuff.StoreClerk("And Lastly, Seniors?");
+                FancyStuff.Typehere();
+                int NumberofSeniors = int.Parse(System.Console.ReadLine());
+
+                int TotalNumberofParty = NumberofKids + NumberofAdults + NumberofSeniors;
+                double CostOfKidsTickets = NumberofKids * 3.99;
+                double CostOfAdultTickets = NumberofAdults * 5.99;
+                double CostofSeniorTickets = NumberofSeniors * 4.50;
+                double CostOfTotalTickets = CostOfKidsTickets + CostofSeniorTickets + CostOfAdultTickets;
+
+                FancyStuff.StoreClerk("Okay. So far, I have " + NumberofKids + " kids, " + NumberofAdults + " adults, and " + NumberofSeniors + " seniors.");
+                FancyStuff.StoreClerk("Cool. What would you be having today to eat?");
+                FancyStuff.StoreClerk("Would you like a small Soda?");
+                int NumberofSmallSoda = int.Parse(System.Console.ReadLine());
+                FancyStuff.StoreClerk("Prefer a large Soda");
+                int NumberofLargeSoda = int.Parse(System.Console.ReadLine());
+                FancyStuff.StoreClerk("Okay. Now for food. Would you like a hotdog?");
+                int NumberOfHotdog = int.Parse(System.Console.ReadLine());
+                FancyStuff.StoreClerk("All right. What about some popcorn?");
+                int NumberofPopcorn = int.Parse(System.Console.ReadLine());
+                FancyStuff.StoreClerk("Lastly, would you like some candy?");
+                int NumberofCandies = int.Parse(System.Console.ReadLine());
+
+
+
+            }
+
+            else if (Answer.ToLower().Contains("night") || Answer.ToLower().Contains("evening"))
+                {
+
+
+            }
+                
+
+            
+            else
+            {
+                FancyStuff.StoreClerk("I don't believe that was what I asked...");
+            }
+
+
+        }
+    }
+}
